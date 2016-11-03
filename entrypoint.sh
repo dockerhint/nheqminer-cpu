2e2b4a13580ec95fc9623ccd27ff568ef211481b
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2016-11-03 15:41:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,11 @@ set -e
 
 for arg in "$@" ; do
   if [[ $arg == "-p"* ]] ; then
-    exec gosu nicehash /usr/local/bin/nheqminer "$@"
+    exec /usr/bin/nheqminer "$@"
   elif [[ $arg == "-h"* ]] ; then
-    exec gosu nicehash /usr/local/bin/nheqminer -h
+    exec /usr/bin/nheqminer -h
   fi
 done
 
-password="$(< /dev/urandom tr -dc 'A-Z-a-z-0-99!#$%&()*+,-./:;<=>?@[\]^_{|}~' | head -c 64)"
-exec gosu nicehash /usr/local/bin/nheqminer "$@" \
-  -p "$password"
+exec /usr/bin/nheqminer "$@"
+
